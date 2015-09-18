0f8b18bb106cc64db8ccb0d0484cc5d665aec5de
maddinthe <martintheilen@gmail.com>
2015-09-18 10:39:19 +0200

    Hausaufgabe fertig

---
+++ src/HexEditor.java
@@ -0,0 +1,41 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by mtheilen on 18.09.2015.
+ */
+public class HexEditor {
+    public static void main(String[] args) {
+
+        try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream("Download.png"))) {
+
+            byte[] b = new byte[16];
+            int c = 0;
+            int addr=0;
+            while ((c = fis.read(b)) != -1) {
+                String front=Integer.toHexString(addr).toUpperCase();
+                while (front.length()<6){
+                    front=0+front;
+                }
+                System.out.print(front+": ");
+                for (int i = 0; i < c; i++) {
+                    int hexb = b[i] & 255;
+                    String hex = Integer.toHexString(hexb).toUpperCase();
+                    if (hex.length() == 1) hex = 0 + hex;
+                    System.out.print(hex + " ");
+                }
+                System.out.print(new String(b).replaceAll("[^\\p{Print}]","."));
+                addr+=16;
+                System.out.println();
+
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Dateifehler");
+        }
+
+
+    }
+}
---
+++ src/TestRWBild.java
@@ -0,0 +1,32 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * Created by mtheilen on 18.09.2015.
+ */
+public class TestRWBild {
+    public static void main(String[] args) {
+
+        try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream("Download.png"))) {
+
+            byte[] b = new byte[16];
+            int c = 0;
+            while ((c = fis.read(b)) != -1) {
+                System.out.println();
+                for (int i = 0; i < c; i++) {
+                    int hexb = b[i]&255;
+                    String hex = Integer.toHexString(hexb).toUpperCase();
+                    if (hex.length() == 1) hex = 0 + hex;
+                    System.out.print(hex + " ");
+                }
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Dateifehler");
+        }
+
+
+    }
+}
