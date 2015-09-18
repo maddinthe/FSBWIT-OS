b673996a437a019228b98b9528144360e6669a39
maddinthe <martintheilen@gmail.com>
2015-09-18 09:15:36 +0200

    dinge mit lesen und schreiben

--- src/TestRW.java
+++ src/TestRW.java
@@ -1,5 +1,12 @@
 import java.io.*;
-import java.io.BufferedReader;import java.io.BufferedWriter;import java.io.FileReader;import java.io.FileWriter;import java.io.IOException;import java.lang.String;import java.lang.System;import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.String;
+import java.lang.System;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,21 +14,19 @@ import java.util.List;
  */
 public class TestRW {
     public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader("Nationalhymne.txt"));
+        try (BufferedReader br = new BufferedReader(new FileReader("Nationalhymne.txt")); BufferedWriter bw = new BufferedWriter(new FileWriter("neue.txt"));) {
+
 
             String zeile = null;
             List<String> al = new ArrayList<>();
             while ((zeile = br.readLine()) != null)
                 al.add(zeile);
 
-            BufferedWriter bw = new BufferedWriter(new FileWriter("neue.txt"));
 
-            for (String s : al) {
-                bw.write(s);
+            for (int i = al.size() - 1; i >= 0; i--) {
+                bw.write(al.get(i));
                 bw.newLine();
             }
-            bw.close();
 
 
         } catch (IOException e) {
