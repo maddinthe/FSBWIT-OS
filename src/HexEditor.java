b65302912173d06ef45e33eed67acdcfc022c2d6
maddinthe <martintheilen@gmail.com>
2015-09-21 09:40:45 +0200

    Hausaufgabe fertig und nochmal verbessert

--- src/HexEditor.java
+++ src/HexEditor.java
@@ -2,27 +2,29 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * Created by mtheilen on 18.09.2015.
  */
 public class HexEditor {
     public static void main(String[] args) {
-        File f=new File("Download.png");
+        File f=new File("java-insel.jpg");
 
         try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream(f))) {
 
             byte[] b = new byte[16];
             int c = 0;
             int addr=0;
-            int maxAddr=Long.toHexString(f.length()).length();  //reicht für 16gb große dateien
+            Long restLength=f.length();
+            int maxAddrLength=Long.toHexString(restLength).length();  //reicht für 16gb große dateien
             while ((c = fis.read(b)) != -1) {
                 String front=Integer.toHexString(addr).toUpperCase();
-                while (front.length()<maxAddr){
+                while (front.length()<maxAddrLength){
                     front=0+front;
                 }
                 System.out.print(front+": ");
-                int count=1;
+                int count=0;
                 for (int i = 0; i < c; i++) {
                     int hexb = b[i] & 255;
                     String hex = Integer.toHexString(hexb).toUpperCase();
@@ -30,13 +32,16 @@ public class HexEditor {
                     System.out.print(hex + " ");
                     count++;
                 }
-                while (count<16)
+                while (count<16){
                     System.out.print("   ");
+                count++;}
+
                 System.out.print(new String(b).replaceAll("[^\\p{Print}]","."));
                 addr+=16;
                 System.out.println();
-
-            }
+                restLength--;
+                if(restLength==1)
+                Arrays.fill(b,(byte)0);            }
 
 
         } catch (IOException e) {
