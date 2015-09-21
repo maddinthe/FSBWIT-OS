6ed6455dfb8632247cdf68d7037b1a175147e8f5
maddinthe <martintheilen@gmail.com>
2015-09-21 10:28:26 +0200

    Hausaufgabe fertig und nochmal besser verbessert

--- src/HexEditor.java
+++ src/HexEditor.java
@@ -9,39 +9,41 @@ import java.util.Arrays;
  */
 public class HexEditor {
     public static void main(String[] args) {
-        File f=new File("java-insel.jpg");
+        File f = new File("java-insel.jpg");
 
         try (BufferedInputStream fis = new BufferedInputStream(new FileInputStream(f))) {
 
             byte[] b = new byte[16];
             int c = 0;
-            int addr=0;
-            Long restLength=f.length();
-            int maxAddrLength=Long.toHexString(restLength).length();  //reicht für 16gb große dateien
+            int addr = 0;
+            long restLength = f.length();
+            int maxAddrLength = Long.toHexString(restLength).length();
             while ((c = fis.read(b)) != -1) {
-                String front=Integer.toHexString(addr).toUpperCase();
-                while (front.length()<maxAddrLength){
-                    front=0+front;
+                String front = Integer.toHexString(addr).toUpperCase();
+                while (front.length() < maxAddrLength) {
+                    front = 0 + front;
                 }
-                System.out.print(front+": ");
-                int count=0;
+                System.out.print(front + ": ");
+                int count = 0;
                 for (int i = 0; i < c; i++) {
                     int hexb = b[i] & 255;
                     String hex = Integer.toHexString(hexb).toUpperCase();
                     if (hex.length() == 1) hex = 0 + hex;
                     System.out.print(hex + " ");
                     count++;
+                    restLength--;
                 }
-                while (count<16){
+                while (count < 16) {
                     System.out.print("   ");
-                count++;}
+                    count++;
+                }
 
-                System.out.print(new String(b).replaceAll("[^\\p{Print}]","."));
-                addr+=16;
+                System.out.print(new String(b).replaceAll("[^\\p{Print}]", "."));
+                addr += 16;
                 System.out.println();
-                restLength--;
-                if(restLength==1)
-                Arrays.fill(b,(byte)0);            }
+                if (restLength < 16)
+                    Arrays.fill(b, (byte) 0);
+            }
 
 
         } catch (IOException e) {
