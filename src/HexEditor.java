85d7a2191e745425cf83f64616fc46ac951f4ea1
maddinthe <martintheilen@gmail.com>
2015-09-21 08:59:06 +0200

    Hausaufgabe fertig und verbessert

--- src/HexEditor.java
+++ src/HexEditor.java
@@ -15,19 +15,23 @@ public class HexEditor {
             byte[] b = new byte[16];
             int c = 0;
             int addr=0;
-            int maxAddr=((Long)f.length()).toString().length()-1;
+            int maxAddr=Long.toHexString(f.length()).length();  //reicht für 16gb große dateien
             while ((c = fis.read(b)) != -1) {
                 String front=Integer.toHexString(addr).toUpperCase();
                 while (front.length()<maxAddr){
                     front=0+front;
                 }
                 System.out.print(front+": ");
+                int count=1;
                 for (int i = 0; i < c; i++) {
                     int hexb = b[i] & 255;
                     String hex = Integer.toHexString(hexb).toUpperCase();
                     if (hex.length() == 1) hex = 0 + hex;
                     System.out.print(hex + " ");
+                    count++;
                 }
+                while (count<16)
+                    System.out.print("   ");
                 System.out.print(new String(b).replaceAll("[^\\p{Print}]","."));
                 addr+=16;
                 System.out.println();
