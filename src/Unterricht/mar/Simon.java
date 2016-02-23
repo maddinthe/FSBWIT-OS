dc3f27588bccd808fadc875b37db03c3a8750241
maddinthe <martintheilen@gmail.com>
2016-02-23 13:25:31 +0100

    ue 22.01.16

---
+++ src/Unterricht/mar/Simon.java
@@ -0,0 +1,28 @@
+package Unterricht.mar;
+
+import javax.swing.*;
+
+/**
+ * Created by mtheilen on 23.02.2016.
+ */
+public class Simon extends JFrame {
+
+
+    public Simon(){
+        setTitle("Simon");
+
+
+
+
+        pack();
+        setVisible(true);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        new Simon();
+    }
+}
